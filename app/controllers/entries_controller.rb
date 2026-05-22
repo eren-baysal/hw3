f93f72649fb293152d51914d17bc0953a20c0c75
eren-baysal <eren.baysal@kellogg.northwestern.edu>
2026-05-22 03:05:30 +0000

    Final adjustments

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,4 @@
-class EntrieController < ApplicationController
+class EntriesController < ApplicationController
   def new
     @place = Place.find_by({ "id" => params["place_id"] })
   end
@@ -7,7 +7,7 @@ class EntrieController < ApplicationController
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
-    @entry["posted_on"] = params["posted_on"]
+    @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry.save
     
